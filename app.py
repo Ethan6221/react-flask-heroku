e599719a1a7fa4a7e5f71ad76686b91248efdf99
Ethan Pham <ethan.pham@utexas.edu>
2022-03-28 17:47:12 -0500

    creating app ready for heroku deployment

---
+++ app.py
@@ -0,0 +1,31 @@
+from flask import Flask, jsonify
+from flask.helpers import send_from_directory
+
+# comment out on deployment
+# from flask_cors import CORS
+
+# uses 'frontend' because that is where our react app is stored
+app = Flask(__name__, static_folder="frontend/build", static_url_path="")
+
+# comment out on deployment
+# CORS(app)
+
+
+# this method returns the opposite of the current state of the button
+# this would look different for your own personal app
+@app.route("/toggle_button/<button_state>", methods=["GET"])
+def toggle_button(button_state: str):
+    if button_state == "Off":
+        output = "On"
+    else:
+        output = "Off"
+    return jsonify(button=output)
+
+
+@app.route("/")
+def index():
+    return send_from_directory(app.static_folder, "index.html")
+
+
+if __name__ == "__main__":
+    app.run(host="0.0.0.0")
---
+++ frontend/src/MyComponent.js
@@ -0,0 +1,29 @@
+import React, { useState } from "react";
+import Button from "@mui/material/Button"
+
+export default function MyComponent() {
+    const [toggleButton, setButton] = useState("Off");
+
+    return(
+        <div>
+            <p>This is a button that you can toggle on and off</p>
+            <Button
+                variant="outlined"
+                onClick={() => {
+                    fetch("/toggle_button/" + toggleButton)
+                        .then(response =>
+                            response.json()
+                        )
+                        .then(data => {
+                            setButton(data.button)
+                        })
+                        .catch(error => {
+                            console.log(error)
+                        })
+                }}
+            >
+                {toggleButton}
+            </Button>
+        </div>
+    );
+}
